64723986cf79f42893b62f2fce610b555496252f
Anurag-dma <anuragshuklax@gmail.com>
2023-06-19 16:48:21 +0530

    authenticate using passport js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -8,41 +8,57 @@ module.exports.profile = function(req,res){
 } 
 
 module.exports.SignUp = function(req,res){
+     if (req.isAuthenticated()){
+          return res.redirect('/users/profile');
+      }
      return res.render('user_sign_up', {
           title:"Codial | sign Up"
      })
 }
 module.exports.SignIn = function(req,res){
+     if (req.isAuthenticated()){
+          return res.redirect('/users/profile');
+      }
      return res.render('user_sign_in', {
           title:"Codial | sign In"
      })
-     User.findone()
+
 }
 
-/* get the Sign Up Data */
-
-module.exports.create = function(req,res){
-     if(req.body.password != req.body.confirm_password){
-          return res.redirect('back');
+module.exports.create = async function(req, res) {
+     if (req.body.password !== req.body.confirm_password) {
+       return res.redirect('back');
      }
-     User.findOne({email: req.body.email}, function(err, user){
-          if(err){console.log('error in finding user in signing up'); return}
-  
-          if (!user){
-              User.create(req.body, function(err, user){
-                  if(err){console.log('error in creating user while signing up'); return}
-  
-                  return res.redirect('/users/sign-in');
-              })
-          }else{
-              return res.redirect('back');
-          }
-      } );
-
-}
-
+   
+     try {
+       const existingUser = await User.findOne({ email: req.body.email });
+   
+       if (!existingUser) {
+         const newUser = await User.create(req.body);
+         return res.redirect('/users/sign-in');
+       } else {
+         return res.redirect('back');
+       }
+     } catch (error) {
+       console.log('Error in signing up:', error);
+       return res.redirect('back');
+     }
+   };
+   
 /* get the Sign In Data */
 
 module.exports.createSession = function(req,res){
-     
+     return res.redirect('/')
 }
+
+module.exports.destroySession = function(req, res){
+     req.logout(function(err) {
+          if (err) {
+            // Handle the error
+            console.log(err);
+            return res.status(500).send('Internal Server Error');
+           }
+           } );
+ 
+     return res.redirect('/');
+ }
--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+//used for seddion cookie
+const session = require('express-session')
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo');
 
 app.use(express.urlencoded());
 app.use(cookieParser());
@@ -14,9 +19,39 @@ app.use(expressLayouts);
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-app.use('/', require('./routes'));
-app.set('view engine','ejs');
+// set up the view engine
+app.set('view engine', 'ejs');
 app.set('views', './views');
+
+// mongo store is used to store the session cookie in the db
+app.use(session({
+    name: 'codeial',
+    // TODO change the secret before deployment in production mode
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+    store: MongoStore.create(
+        {
+            mongoUrl: 'mongodb://127.0.0.1/codial_development',
+            autoRemove: 'disabled'
+        
+        },
+        function(err){
+            console.log(err ||  'connect-mongodb setup ok');
+        }
+    )
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.setAuthenticatedUser);
+
+// use express router
+app.use('/', require('./routes'));
+
 app.listen(port,(err) =>{
                
     if(err)
