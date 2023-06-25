12990c7e0850211ae0d092f1503824cfdc0d3544
Anurag-dma <anuragshuklax@gmail.com>
2023-06-26 00:15:48 +0530

    display posts on user home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,30 @@
-module.exports.home = function(req,res){
-    console.log(req.cookies);
-    res.cookie('user_id', 87);
-    res.cookie('user', 90);
-    return res.render('home', {
-        title: "Home"
-    });
-}
+const Post = require('../models/post');
+
+
+
+module.exports.home = async function (req, res) {
+    // try {
+    //     const posts = await Post.find({});
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts: posts
+    //     });
+    // } catch (err) {
+    //     // Handle the error
+    //     console.error(err);
+    //     return res.status(500).send("Internal Server Error");
+    // }
+
+    // populate the user of each post
+    try {
+        const posts = await Post.find({}).populate('user').exec();
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        });
+    } catch (err) {
+        // Handle the error
+        console.error(err);
+        return res.status(500).send("Internal Server Error");
+    }
+};
