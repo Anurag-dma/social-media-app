56042b62d976ed5938e0c3f3a945f2a8724a5cc0
Anurag-dma <anuragshuklax@gmail.com>
2023-06-26 23:30:26 +0530

    add post and comment delete button

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -22,3 +22,25 @@ module.exports.create = async function (req, res) {
         return res.status(500).send("Internal Server Error");
     }
 };
+
+
+module.exports.destroy = async function (req, res) {
+    try {
+        const comment = await Comment.findById(req.params.id);
+        if (comment.user == req.user.id) {
+            const postId = comment.post;
+    
+            await comment.deleteOne();
+    
+            await Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } });
+    
+            return res.redirect('back');
+        } else {
+            return res.redirect('back');
+        }
+    } catch (err) {
+        // Handle the error
+        console.error(err);
+        return res.status(500).send("Internal Server Error");
+    }
+};
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = function (req, res) {
   Post.create({
@@ -12,4 +13,25 @@ module.exports.create = function (req, res) {
       console.log('Error in creating a post:', err);
       return res.redirect('back');
     });
+};
+
+
+module.exports.destroy = async function (req, res) {
+    try {
+        const post = await Post.findById(req.params.id);
+        // .id means converting the object id into string
+        if (post.user == req.user.id) {
+            await post.deleteOne();
+    
+            await Comment.deleteMany({ post: req.params.id });
+    
+            return res.redirect('back');
+        } else {
+            return res.redirect('back');
+        }
+    } catch (err) {
+        // Handle the error
+        console.error(err);
+        return res.status(500).send("Internal Server Error");
+    }
 };
