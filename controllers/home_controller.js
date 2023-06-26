79591a01e97db8ee600dc1b7fbb9cb7c2dc5c25e
Anurag-dma <anuragshuklax@gmail.com>
2023-06-26 12:56:43 +0530

    create comment schema and add comment to users post and display it

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -17,7 +17,16 @@ module.exports.home = async function (req, res) {
 
     // populate the user of each post
     try {
-        const posts = await Post.find({}).populate('user').exec();
+        const posts = await Post.find({})
+            .populate('user')
+            .populate({
+                path: 'comments',
+                populate: {
+                    path: 'user'
+                }
+            })
+            .exec();
+            
         return res.render('home', {
             title: "Codeial | Home",
             posts: posts
