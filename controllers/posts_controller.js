7f559c390e77805676db2366aba07dce2d4a9b8b
Anurag-dma <anuragshuklax@gmail.com>
2023-07-07 23:07:27 +0530

    use ajax to create and view user posts

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,20 +1,32 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 
-module.exports.create = function (req, res) {
-  Post.create({
-    content: req.body.content,
-    user: req.user._id
-  })
-    .then((post) => {
+module.exports.create = async function(req, res){
+  try{
+      let post = await Post.create({
+          content: req.body.content,
+          user: req.user._id
+      });
+      
+      if (req.xhr){
+    
+          return res.status(200).json({
+              data: {
+                  post: post
+              },
+              message: "Post created!"
+          });
+      }
+
       req.flash('success', 'Post published!');
       return res.redirect('back');
-    })
-    .catch((err) => {
+
+  }catch(err){
       req.flash('error', err);
       return res.redirect('back');
-    });
-};
+  }
+
+}
 
 
 module.exports.destroy = async function (req, res) {
