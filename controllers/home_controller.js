dc4d115554cbb06840d76545c776a8b22de1f4bf
Anurag-dma <anuragshuklax@gmail.com>
2023-07-11 22:16:59 +0530

    add delte post functionality using ajax

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,39 +1,30 @@
 const Post = require('../models/post');
 const User = require('../models/user');
+module.exports.home = async function(req, res){
 
-module.exports.home = async function (req, res) {
-
-    // console.log(req.cookies);
-    // res.cookie('user_id', 25);
-
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: "Codeial | Home",
-    //         posts:  posts
-    //     });
-    // });
-
-    // populate the user of each post
-  try {
-    const posts = await Post.find({})
+  try{
+       // populate the user of each post
+      let posts = await Post.find({})
+      .sort('-createdAt')
       .populate('user')
       .populate({
-        path: 'comments',
-        populate: {
-          path: 'user'
-        }
-      })
-      .exec();
+          path: 'comments',
+          populate: {
+              path: 'user'
+          }
+      });
+  
+      let users = await User.find({});
 
-    const users = await User.find({}).exec();
+      return res.render('home', {
+          title: "Codeial | Home",
+          posts:  posts,
+          all_users: users
+      });
 
-    return res.render('home', {
-      title: "Codeial | Home",
-      posts: posts,
-      all_users: users
-    });
-  } catch (err) {
-    console.log('Error in fetching posts or users:', err);
-    return res.redirect('back');
+  }catch(err){
+      console.log('Error', err);
+      return;
   }
-};
+ 
+}
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -28,23 +28,37 @@ module.exports.create = async function(req, res){
 
 }
 
+module.exports.destroy = async function(req, res){
 
-module.exports.destroy = async function (req, res) {
-    try {
-        const post = await Post.findById(req.params.id);
-        // .id means converting the object id into string
-        if (post.user == req.user.id) {
-            await post.deleteOne();
-    
-            await Comment.deleteMany({ post: req.params.id });
+    try{
+        let post = await Post.findById(req.params.id);
+
+        if (post.user == req.user.id){
+            post.deleteOne();
+
+            await Comment.deleteMany({post: req.params.id});
+
+
+            if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        post_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
+
             req.flash('success', 'Post and associated comments deleted!');
+
             return res.redirect('back');
-        } else {
+        }else{
+            req.flash('error', 'You cannot delete this post!');
             return res.redirect('back');
         }
-    } catch (err) {
-        // Handle the error
-        req.flash('error', 'You cannot delete this post!');
-        return res.status(500).send("Internal Server Error");
+
+    }catch(err){
+        req.flash('error', err);
+        return res.redirect('back');
     }
-};
+    
+}
